94aef7ec86ec9bdb3472f06e29e717da35fc57ea
JacksonBaltramovich <e.baltramovich@mail.ru>
2022-05-23 21:01:51 +0300

    feat: complete copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require("fs/promises");
+const path = require("path");
 
+const myPath = path.join(__dirname, "files");
+
+async function copyDir() {
+
+    await fs.mkdir(path.join(__dirname, "files-copy"), {recursive:true});
+
+    const data = await fs.readdir(myPath);
+
+
+    data.forEach(async (file) => {
+        const sourceFile = path.join(__dirname, "files", file);
+        const newFile = path.join(__dirname, "files-copy", file);
+
+        await fs.copyFile(sourceFile, newFile)
+        .catch(function(err) {
+            console.log(err);
+        })
+
+    });
+}
+
+copyDir();
